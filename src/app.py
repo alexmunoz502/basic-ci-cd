8b5622715d180a7ae24323d4710a4d5f37990f84
Alex Munoz <alex@munoz.us>
2024-01-08 20:00:34 -0500

    initial commit

---
+++ src/app.py
@@ -0,0 +1,14 @@
+from litestar import Litestar, get
+
+
+@get("/")
+async def index() -> str:
+    return "Hello, World!"
+
+
+@get("/greeting")
+async def greeting(name: str) -> str:
+    return f"Hello, {name.title()}!"
+
+
+app = Litestar([index, greeting])
---
+++ tests/test_index.py
@@ -0,0 +1,23 @@
+import pytest
+from litestar.testing import TestClient
+from litestar.status_codes import HTTP_200_OK
+from src.app import app
+
+
+@pytest.fixture(scope="function")
+def test_client() -> TestClient:
+    return TestClient(app=app)
+
+
+def test_index_message(test_client: TestClient) -> None:
+    with test_client as client:
+        response = client.get("/")
+        assert response.status_code == HTTP_200_OK
+        assert response.text == "Hello, World!"
+
+
+def test_greeting_message(test_client: TestClient) -> None:
+    with test_client as client:
+        response = client.get("/greeting?name=alex")
+        assert response.status_code == HTTP_200_OK
+        assert response.text == "Hello, Alex!"
