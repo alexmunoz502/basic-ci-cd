fc85d0bd4b8a2263fcb6fc6c097d5dcc643a3f1b
Alex Munoz <alex@munoz.us>
2024-01-10 09:58:02 -0500

    updated to use port from env var

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import uvicorn
+import os
 from litestar import Litestar, get
 
 
@@ -13,3 +14,7 @@ async def greeting(name: str) -> str:
 
 
 app = Litestar([index, greeting])
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
