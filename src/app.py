732f1791dc2623b41b39c0f0b1e746a94770af3f
Alex Munoz <alex@munoz.us>
2024-01-10 10:04:43 -0500

    updated to use app string instead of obj

--- src/app.py
+++ src/app.py
@@ -17,4 +17,4 @@ app = Litestar([index, greeting])
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    uvicorn.run("app:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
